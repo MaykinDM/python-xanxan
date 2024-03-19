01306eb264d0621d10e40edc915cf748b1049f77
Maycon Douglas <126088437+MaykinDM@users.noreply.github.com>
2024-03-18 22:49:37 -0300

    Add files via upload

---
+++ python-xanxan-main/python-xanxan-main/may-python-3ds-24/Ex1Trimeste/adivinhacao.py
@@ -0,0 +1,15 @@
+    import random
+    print("***************************************")
+    print("BEM VINDO AO JOGO DE ADIVINHAÇÃO")
+    print("***************************************")
+    numero_secreto = random.randint ( 1, 100)
+    total_de_tentativas = 0
+    rodada = 1
+    pontos = 1000
+
+    print("Qual o nível de dificuldade?")
+    print("(1) Fácil  -  (2) Médio  -  (3) Difícil")
+    nível = int(input("Defina o Nível: "))
+    if(nível == 1)
+        
+
---
+++ python-xanxan-main/python-xanxan-main/may-python-3ds-24/Ex1Trimeste/ex01-02.py
@@ -0,0 +1,5 @@
+print("Programa para caucular a área de triângulo")
+base = float(input("Entre com a base do triângulo: "))
+altura = float(input("Entre com a altura do triângulo: "))
+area = (base * altura)/2
+print("A área do triângulo é: ", area)
